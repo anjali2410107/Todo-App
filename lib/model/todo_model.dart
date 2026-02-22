4bf3fb72a1b5ed1d5cdaf479451d5d5cec4c5fa9
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-02-22 21:38:30 +0530

    Updated model

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -7,6 +7,21 @@ class TodoModel {
     required this.id,
     required this.title,
     this.isCompleted=false,
+});
+Map<String,dynamic> toMap()
+{
+  return{
+    'id':id,
+    'title':title,
+    'isComplete':isCompleted,
+  };
 }
-       );
+factory TodoModel.fromMap(Map map)
+  {
+    return TodoModel(
+        id: map['id'],
+        title: map['title'],
+    isCompleted: map['isCompleted'],
+    );
+  }
 }
