80dc18ae938b4e73a638ca60ea86e061c3bf9506
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-02-22 23:58:46 +0530

    added methods on homescreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:todoappp/main.dart';
+import 'package:todoappp/model/todo_model.dart';
+import 'package:todoappp/repository/todo_repository.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TodoRepository repository = TodoRepository();
+  final TextEditingController controller = TextEditingController();
+  final uuid = const Uuid();
+
+  List<TodoModel> todos = [];
+
+  void initState()
+  {
+    super.initState();
+      loadTodos();
+    }
+
+
+  void loadTodos() {
+    todos = repository.getTodos();
+    setState(() {});
+  }
+    void addTodo() async
+    {
+      if(controller.text.isEmpty) return;
+      final todo=TodoModel(id: uuuid.v4(),
+          title: controller.text,);
+      await repository.addTodo(todo);
+      controller.clear();
+      loadTodos();
+    }
+
+    void deleteTodo(String id) async
+    {
+      await repository.deleteTodo(id);
+      loadTodos();
+    }
+
+    void toggleTodo(TodoModel todo)async
+    {
+      await repository.toString(todo);
+      loadTodos();
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
