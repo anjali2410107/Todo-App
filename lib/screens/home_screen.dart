acee8d805d6f435f09e26f9934c2e9ec2e6594eb
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-02-23 19:50:22 +0530

    Updated homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todoappp/model/todo_model.dart';
 import 'package:todoappp/repository/todo_repository.dart';
+import 'package:todoappp/todo/todo_bloc.dart';
 import 'package:uuid/uuid.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,44 +13,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TodoRepository repository = TodoRepository();
   final TextEditingController controller = TextEditingController();
-  final Uuid uuid =  Uuid();
 
-  List<TodoModel> todos = [];
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
-      loadTodos();
-    }
-
-
-  void loadTodos() {
-    todos = repository.getTodos();
-    setState(() {});
+    context.read<TodoBloc>().add(LoadTodos());
   }
-    void addTodo() async
-    {
-      if(controller.text.isEmpty) return;
-      final todo=TodoModel(id: uuid.v4(),
-          title: controller.text,);
-      await repository.addTodo(todo);
-      controller.clear();
-      loadTodos();
-    }
-
-    void deleteTodo(String id) async
-    {
-      await repository.deleteTodo(id);
-      loadTodos();
-    }
-
-    void toggleTodo(TodoModel todo)async
-    {
-      await repository.toggleTodo(todo);
-      loadTodos();
-    }
 
   @override
   Widget build(BuildContext context) {
@@ -59,45 +31,65 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Padding(padding: const EdgeInsets.all(12),
-          child: Row(
-            children: [
-              Expanded(child: TextField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  hintText: "Enter Todo",
-                border: OutlineInputBorder()
+            child: Row(
+              children: [
+                Expanded(child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                      hintText: "Enter Todo",
+                      border: OutlineInputBorder()
+                  ),
                 ),
-              ),
-              ),
-              const SizedBox(width: 10,),
-              ElevatedButton(onPressed: addTodo, child: const Text("Add"),
-              )
-            ],
-          ),
+                ),
+                const SizedBox(width: 10,),
+                ElevatedButton(onPressed: () {
+                  if (controller.text.isNotEmpty) {
+                    context.read<TodoBloc>().add(
+                      AddTodo(controller.text),
+                    );
+                    controller.clear();
+                  }
+                },
+                  child: const Text("Add"),
+                )
+              ],
+            ),
           ),
           Expanded(child:
-          ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context,index)
-    {
-      final todo=todos[index];
-      return ListTile(
-    leading: Checkbox(value: todo.isCompleted,
-    onChanged: (_) =>toggleTodo(todo),
-    ),
-    title: Text(todo.title,
-    style: TextStyle(
-    decoration: todo.isCompleted? TextDecoration.lineThrough:
-    TextDecoration.none,
-    ),),
-    trailing: IconButton(onPressed: () =>deleteTodo(todo.id),
-    icon: const Icon(Icons.delete),
-    ),
-
-    );
-
-    },
-    ),
+          BlocBuilder<TodoBloc, TodoState>(
+            builder: (context, state) {
+              if (state is TodoLoaded) {
+                return ListView.builder(
+                  itemCount: state.todos.length,
+                  itemBuilder: (context, index) {
+                    final todo = state.todos[index];
+                    return ListTile(
+                      leading: Checkbox
+                        (value: todo.isCompleted,
+                        onChanged: (_) {
+                          context:
+                          context.read<TodoBloc>().add(ToggleTodo(todo),
+                          );
+                        },),
+                      title:
+                      Text(todo.title,
+                        style: TextStyle(
+                          decoration: todo.isCompleted ? TextDecoration
+                              .lineThrough :
+                          TextDecoration.none,
+                        ),),
+                      trailing: IconButton(onPressed: () {
+                        context.read<TodoBloc>().add(DeleteTodo(todo.id));
+                      },
+                        icon: const Icon(Icons.delete),
+                      ),
+                    );
+                  },
+                );
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
           )
         ],
       ),
