284342fa21a3373cdec5d9ff9bcbe6cc72685979
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-02-23 20:00:37 +0530

    Provided Bloc in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todoappp/repository/todo_repository.dart';
+import 'package:todoappp/todo/todo_bloc.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('todos');
-  runApp(const MyApp());
+
+final repository=TodoRepository();
+  runApp(BlocProvider(create: (_) => TodoBloc(repository),
+  child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
