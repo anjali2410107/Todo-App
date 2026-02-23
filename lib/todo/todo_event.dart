828498ed84dd2eaf4c7391073bef2a73c443f519
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-02-23 19:20:34 +0530

    added classes in todoevents

--- lib/todo/todo_event.dart
+++ lib/todo/todo_event.dart
@@ -1,4 +1,18 @@
 part of 'todo_bloc.dart';
 
-@immutable
-sealed class TodoEvent {}
+abstract class TodoEvent {}
+class LoadTodos extends TodoEvent{}
+ class AddTodo extends TodoEvent
+ {
+   final String title;
+   AddTodo(this.title);
+ }
+ class DeleteTodo extends TodoEvent{
+  final String id;
+  DeleteTodo(this.id);
+ }
+  class ToggleTodo extends TodoEvent
+  {
+    final TodoEvent todo;
+    ToggleTodo(this.todo);
+  }
