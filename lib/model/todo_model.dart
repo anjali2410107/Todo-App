df6ee7d40cd0c94e6c875a33f8097906959cafad
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-03-13 16:02:51 +0530

    updated model

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,10 +1,16 @@
-
-enum TaskPriority
-{
+enum TaskPriority {
   high,
   medium,
-  low
+  low,
 }
+
+enum TaskFilter {
+  all,
+  overdue,
+  completed,
+  highPriority,
+}
+
 class TodoModel {
   final String id;
   final String title;
@@ -12,38 +18,62 @@ class TodoModel {
   final TaskPriority priority;
   final DateTime? dueDate;
   final DateTime? startDate;
-   TodoModel
-       (
-  {
+  final String? taskTypeId;
+
+  TodoModel({
     required this.id,
     required this.title,
-    this.isCompleted=false,
-    this.priority=TaskPriority.medium,
+    this.isCompleted = false,
+    this.priority = TaskPriority.medium,
     this.dueDate,
     this.startDate,
-});
-Map<String,dynamic> toMap()
-{
-  return{
-    'id':id,
-    'title':title,
-    'isCompleted':isCompleted,
-    'priority':priority.name,
-    'dueDate':dueDate?.toIso8601String(),
-    'startDate':startDate?.toIso8601String(),
-  };
-}
-factory TodoModel.fromMap(Map<String,dynamic> map)
-  {
+    this.taskTypeId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+      'priority': priority.name,
+      'dueDate': dueDate?.toIso8601String(),
+      'startDate': startDate?.toIso8601String(),
+      'taskTypeId': taskTypeId,
+    };
+  }
+
+  factory TodoModel.fromMap(Map<String, dynamic> map) {
     return TodoModel(
-        id: map['id'],
-        title: map['title'],
-    isCompleted: map['isCompleted']?? false,
-      priority: TaskPriority.values.firstWhere((e)
-        => e.name==map['priority'],orElse: ()=>TaskPriority.medium,
+      id: map['id'],
+      title: map['title'],
+      isCompleted: map['isCompleted'] ?? false,
+      priority: TaskPriority.values.firstWhere(
+            (e) => e.name == map['priority'],
+        orElse: () => TaskPriority.medium,
       ),
-      dueDate: map['dueDate'] !=null?DateTime.parse(map['dueDate']):null,
-      startDate: map['startDate']!=null?DateTime.parse(map['startDate']):null,
+      dueDate: map['dueDate'] != null ? DateTime.parse(map['dueDate']) : null,
+      startDate: map['startDate'] != null ? DateTime.parse(map['startDate']) : null,
+      taskTypeId: map['taskTypeId'],
+    );
+  }
+
+  TodoModel copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+    TaskPriority? priority,
+    DateTime? dueDate,
+    DateTime? startDate,
+    String? taskTypeId,
+  }) {
+    return TodoModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+      priority: priority ?? this.priority,
+      dueDate: dueDate ?? this.dueDate,
+      startDate: startDate ?? this.startDate,
+      taskTypeId: taskTypeId ?? this.taskTypeId,
     );
   }
 }
